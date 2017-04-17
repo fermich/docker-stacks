ff76409693acb508688285f28c4c56126a9e0ff2
Michal Ferlinski <fermich@gmail.com>
2017-04-17 18:18:54 +0200

    sudo on user

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run -v /home/michal/devel/notebooks:/home/jovyan/work -it --rm -p 8888:8888 jupyter/datascience-notebook
+docker run -v /home/michal/devel/invest-notebooks:/home/jovyan/work --user root -e GRANT_SUDO=yes -it --rm -p 8888:8888 jupyter/datascience-notebook
 
